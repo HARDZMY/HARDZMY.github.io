7f349d9269aed7a65d5992cbe473cc9f1db7ec14
Hazmi Hashim <MuhdHazmiAdli.MohdHashim@sony.com>
2024-08-19 17:30:03 +0800

    test

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
+// Navigation Bar Active Page
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const link = document.querySelector(`nav ul li a[href="#${id}"]`);
+
+            if (entry.isIntersecting) {
+                navLinks.forEach(link => link.classList.remove('active'));
+                link.classList.add('active');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
 // Footer
 var currentYear = new Date().getFullYear();
 var yearText = currentYear + " | All rights reserved | HARDZMY";
