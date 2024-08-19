798e74fa0d9ee267f193ea2e61d747836e78c05b
HARDZMY <hazmihashim11@gmail.com>
2024-08-19 19:33:36 +0800

    test

--- script.js
+++ script.js
@@ -15,15 +15,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const link = document.querySelector(nav ul li a[href="#${id}"]);
+
             if (entry.isIntersecting) {
-                const id = entry.target.getAttribute('id');
-                const activeLink = document.querySelector(`nav ul li a[href="#${id}"]`);
-
-                // Remove active class from all links
                 navLinks.forEach(link => link.classList.remove('active'));
-
-                // Add active class to the current section link
-                activeLink.classList.add('active');
+                link.classList.add('active');
             }
         });
     }, { threshold: 0.5 });
@@ -31,15 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
     sections.forEach(section => {
         observer.observe(section);
     });
-
-    // Handle the click event to remove active class when clicked
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            navLinks.forEach(link => link.classList.remove('active'));
-            link.classList.add('active');
-        });
-    });
 });
+
 
 // Footer
 var currentYear = new Date().getFullYear();
